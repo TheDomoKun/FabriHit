5d8b93a6fa7c51560dbd5f50373ddf3a783410e5
dom0kun__ <burlutskiy.artur@gmail.com>
2022-11-26 12:37:32 +0300

    Inital

---
+++ src/main/java/what/a/fuck/domokun/fabrihit/mixins/ExampleMixin.java
@@ -0,0 +1,16 @@
+package what.a.fuck.domokun.fabrihit.mixins;
+
+import what.a.fuck.domokun.fabrihit.FabriHit;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		FabriHit.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
